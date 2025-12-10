926ea91649e47e93ce8cf64d21b2c4a65567a6de
AlexRovere <wormz91@hotmail.fr>
2025-12-10 14:32:49 +0100

    clean

--- src/greeting.js
+++
@@ -1,8 +0,0 @@
-function getGreeting(name) {
-  if (typeof name === 'string' && name.trim() !== '') {
-    return `Hello ${name}!`;
-  }
-  return "Hello world!";
-}
-
-module.exports = { getGreeting };
